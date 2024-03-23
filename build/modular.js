228d29f05494edccbc043d62add803724c3edd65
cobbdzon <clarkjacob07@gmail.com>
2024-03-23 16:41:29 +0800

    modular dev

--- build/modular.js
+++ build/modular.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const { promisify } = require('util');
 
 const includes_regex = /\[\[(.*?)\]\]/g;
+const dev_script_regex = /<script src=".*modularDev\.js"><\/script>/g
 
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
@@ -18,24 +19,30 @@ readFile("./index.html").catch(console.error).then((data) => {
         filenames[index] = str.slice(2, -2)
     })
 
-    const files = []
+    const element_data_cache = {}
     const promises = []
 
     for (let i = 0; i < filenames.length; i++) {
         const replace_pattern = includes_matches[i];
         const filename = filenames[i];
 
-        promises[i] = readFile('./includes/' + filename).then((data) => {
-            element_data = data.toString()
-            new_html = new_html.replace(replace_pattern, element_data)
-            console.log("libron")
-        }).catch(() => {
-            new_html = new_html.replace(replace_pattern, "Failed to load: " + filename)
-        })
+        if (element_data_cache[filename]) {
+            promises[i] = new Promise(() => {
+                const element_data = element_data_cache[filename]
+                new_html = new_html.replace(replace_pattern, element_data)
+            })
+        } else {
+            promises[i] = readFile('./includes/' + filename).then((data) => {
+                element_data = data.toString()
+                new_html = new_html.replace(replace_pattern, element_data)
+            }).catch(() => {
+                new_html = new_html.replace(replace_pattern, "Failed to load: " + filename)
+            })
+        }
     }
 
     // html has been injected
     Promise.allSettled(promises).then(() => {
-        writeFile("./index.html", new_html)
+        writeFile("./index2.html", new_html)
     })
 })
