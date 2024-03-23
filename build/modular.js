18e2296d58b441517b1ec867cb65720579e8d0ce
cobbdzon <clarkjacob07@gmail.com>
2024-03-23 11:47:05 +0800

    test regex

--- build/modular.js
+++ build/modular.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 
+const replace_regex = /(\[\[.*?\]\])/;
+const filename_regex = /\[\[(.*?)\]\]/;
+
 fs.readFile('./index.html', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    
+    // Transform data Buffer to string.
+    let str_data = data.toString();
+    const includes_matches = str_data.match(replace_regex);
+    const filename_matches = str_data.match(filename_regex);
+    console.log(includes_matches, filename_matches)
 });
